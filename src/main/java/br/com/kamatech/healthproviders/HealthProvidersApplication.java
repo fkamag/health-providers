46ad615e7c70f03a465d8a8fa8a39f8e4a676785
fkamag <fabiokamagian@gmail.com>
2024-04-13 21:49:22 -0300

    starting project

---
+++ src/main/java/br/com/kamatech/healthproviders/HealthProvidersApplication.java
@@ -0,0 +1,13 @@
+package br.com.kamatech.healthproviders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthProvidersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthProvidersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/kamatech/healthproviders/HealthProvidersApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.kamatech.healthproviders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthProvidersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
